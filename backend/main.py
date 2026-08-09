f926c7396305f6398aed61065151223d5e3bb835
saivenkat-954 <saivenkatbommidi701@gmail.com>
2026-08-10 00:19:18 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -32,6 +32,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://document-hub-ai.vercel.app",
     ],
 
     allow_credentials=True,
